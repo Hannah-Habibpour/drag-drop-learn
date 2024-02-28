1d69b9adfe732992ea8961f19bb1d032779f194e
Hannah-Habibpour <hana.habibpor@gmail.com>
2024-02-28 12:40:23 -0500

    Refactor code: Remove unused CSS and update page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
-import { NotionKanban } from "@/components/sections/board/customKanban/notion-kanban";
+"use client";
+
+import { useState } from "react";
 
 export default function Home() {
-  return <NotionKanban />;
+  const [widgets, setWidgets] = useState<string[]>([]);
+
+  function handleOnDrag(e: React.DragEvent, widgetType: string) {
+    e.dataTransfer.setData("widgetType", widgetType);
+  }
+
+  function handleOnDrop(e: React.DragEvent) {
+    const widgetType = e.dataTransfer.getData("widgetType");
+    console.log("widgetType", widgetType);
+    setWidgets([...widgets, widgetType]);
+  }
 }
